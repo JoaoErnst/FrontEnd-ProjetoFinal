93539e2fb53255cb50537f13de5feb86226d2efe
João Ernst <74716479+JoaoErnst@users.noreply.github.com>
2021-02-23 09:55:39 -0300

    Update index.js

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -7,11 +7,10 @@ export default class Home extends Component {
      render(){
          return(
          <div>   
-           <img src="/images/fighter.jpg.png" alt="fighter" />
+         <img src="/images/fighter.jpg.png" alt="fighter" />
          <h2>Home Page</h2>
          <h3>O que é a Luta?</h3>
-         <p>Lutar é uma das atividades esportivas mais antigas. Muito antes de ser considerada como um esporte, a luta tinha o conceito básico de defesa e de ataque, no sentido de demonstrar superioridade em um confronto. Há registros de lutas em praticamente todas as eras da humanidade, passando por babilônicos, egípcios, japoneses, chineses, gregos e romanos, desde milhares de anos antes de Cristo até hoje.
-         Sendo praticada por tanto tempo e em tantos lugares, obviamente há os mais diversos tipos de luta espalhados pelo mundo, sendo difícil determinar exatamente uma origem. Mas os grandes responsáveis pela introdução da modalidade no mundo esportivo foram os gregos. A luta começou a ser disputada nos Jogos da Grécia Antiga no século 7 a.C.. Ao longo dos anos e das edições das Olimpíadas, a modalidade foi evoluindo e ganhando particularidades.</p>
+         <p>Lutar é uma das atividades esportivas mais antigas. Muito antes de ser considerada como um esporte, a luta tinha o conceito básico de defesa e de ataque, no sentido de demonstrar superioridade em um confronto. Há registros de lutas em praticamente todas as eras da humanidade, passando por babilônicos, egípcios, japoneses, chineses, gregos e romanos, desde milhares de anos antes de Cristo até hoje.</p>
          </div> 
          
          );
